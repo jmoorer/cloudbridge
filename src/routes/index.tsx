069ab5f4e702a016b8bb2f654a3a8123cb98c18b
Marlon <marlonmoorer@outlook.com>
2026-04-17 22:43:46 -0400

    added theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "#/components/ui/button";
 import { deleteSession } from "#/lib/api/session";
 import { auth } from "#/lib/auth";
 import { authClient } from "#/lib/auth-client";
@@ -27,19 +28,14 @@ function App() {
     authClient.signIn.social({
       provider: "google",
     });
-  const buttonClassName = "bg-amber-500 px-1 py-2";
   return (
     <div className="p-4">
       <pre>{JSON.stringify({ session: data }, null, 2)}</pre>
 
       {!data ? (
-        <button className={buttonClassName} onClick={() => signIn()}>
-          Sign in
-        </button>
+        <Button onClick={() => signIn()}>Sign in</Button>
       ) : (
-        <button className={buttonClassName} onClick={() => signOut()}>
-          Sign out
-        </button>
+        <Button onClick={() => signOut()}>Sign out</Button>
       )}
     </div>
   );
